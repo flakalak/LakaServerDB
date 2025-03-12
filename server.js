0ecaa78e702bfd8405d07d9c303af0a82760aa89
lakaLakaFou <168801339+lakaLakaFou@users.noreply.github.com>
2025-03-11 22:51:27 -0700

    1st commit to server.js

---
+++ server.js
@@ -0,0 +1,48 @@
+//Lakalaka Laka
+//Information Technology, Arizona State University
+//IFT 458: Middleware Prog & Database Sec
+//Professor: Dinesh Sthapit
+//Due Date: March 16, 2025
+//Lakalaka_9231-403_Module5 Activity 1: Introducing Git with VScode
+
+// Required for Node to read .env file
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+// required for communicating and connecting to the database
+// and perform CRUD operations
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const bookRoutes = require('./routes/bookRoutes');
+
+// required for the application to run
+const app = require('./app');
+// Connect to the database and replace the password
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+// Connect to the database
+//console.log(DB); // check the database connection string
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    //useCreateIndex: true,
+    //useFindAndModify: false
+  })
+  .then(() => {
+    console.log('✅ DB connection successful!🚀')
+  })
+  .catch(err => {
+    console.log('❌ DB connection failed!🚨!');
+    console.log(err); 
+  });
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+  console.log(`To test the IFT 458 REST App Click Or Type: http://localhost:${port}...`);
+});
+
